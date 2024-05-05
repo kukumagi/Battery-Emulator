64c6f8516258de314e4bd72842078a68836520a8
itpr-gwis2 <bitbucket-gwis2@itprojekt.ee>
2024-05-05 13:27:38 +0300

    debug canfd send frame

--- Software/src/battery/KIA-E-GMP-BATTERY.cpp
+++ Software/src/battery/KIA-E-GMP-BATTERY.cpp
@@ -193,6 +193,18 @@ void update_values_battery() {  //This function maps all the values fetched via 
 #endif
 }
 
+void sendCanFd(CANFDMessage frame) {
+  #ifdef DEBUG_VIA_USB+
+  const bool ok = canfd.tryToSend(frame);
+  if (ok) {
+  }else{
+    Serial.println ("Send failure");
+  }
+  #else
+  canfd.tryToSend(frame);
+  #endif
+}
+
 void receive_canfd_battery(CANFDMessage frame) {
   CANstillAlive = 12;
   // Frame ID-s that battery transmits. For debugging and development.
@@ -235,7 +247,7 @@ void receive_canfd_battery(CANFDMessage frame) {
           // Serial.println ("Send ack");
           poll_data_pid = frame.data[4];
           // if (frame.data[4] == poll_data_pid) {
-          canfd.tryToSend(EGMP_7E4_ack);  //Send ack to BMS if the same frame is sent as polled
+          sendCanFd(EGMP_7E4_ack);  //Send ack to BMS if the same frame is sent as polled
           // }
           break;
         case 0x21:  //First frame in PID group
@@ -420,7 +432,7 @@ void send_can_battery() {
     }
     previousMillis500ms = currentMillis;
     EGMP_7E4.data[3] = KIA_7E4_COUNTER;
-    canfd.tryToSend(EGMP_7E4);
+    sendCanFd(EGMP_7E4);
 
     KIA_7E4_COUNTER++;
     if (KIA_7E4_COUNTER > 0x0D) {  // gets up to 0x010C before repeating
